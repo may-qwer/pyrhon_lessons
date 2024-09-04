bc322ec5423d3a22229bcf4a47e0c8b43c942205
may-qwer <may0220@yandex.ru>
2024-09-04 19:56:02 +0300

    page 160

--- lessons/part03.py
+++ lessons/part03.py
@@ -1,4 +1,4 @@
-# import math
+import math
 #
 # #function for calculate exp(x)
 # def fact(n):
@@ -30,13 +30,29 @@
 # print(calculation_expx_2(1, 500))
 # print(math.exp(1))
 
-def solve_eqn(f, x0, n):
-    x = x0
-    for k in range(1, n+1):
-        x = f(x)
-    return x
+# def solve_eqn(f, x0, n):
+#     x = x0
+#     for k in range(1, n+1):
+#         x = f(x)
+#     return x
+#
+# def eqn_1(x):
+#     return (x**2+5)/8
+#
+# print(solve_eqn(eqn_1, 1, 600))
 
-def eqn_1(x):
-    return (x**2+5)/8
+def Fib(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return Fib(n - 1) + Fib(n - 2)
 
-print(solve_eqn(eqn_1, 1, 600))
+for i in range(1, 16):
+    print(Fib(i), end = " ")
+
+def solve_fi(n):
+    return (Fib(n))/(Fib(n-1))
+
+print("")
+print (solve_fi(30))
+print((1 + 5**0.5)/2)
