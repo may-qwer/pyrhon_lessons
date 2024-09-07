fbc3fb71398f10e12ec4db066515147c0fc43e22
may-qwer <may0220@yandex.ru>
2024-09-07 12:23:40 +0300

    page 211

--- lessons/part04.py
+++ lessons/part04.py
@@ -3,9 +3,21 @@ import random
 MAT1 = [[1, 2, 1],
         [4, 2, 2],
         [0, 1, 7]]
+
 MAT2 =  [[7, 5, 1],
         [2, 1, 2],
         [4, 3, 4]]
+
+MAT1_1 = [[5, 3, 7, 0],
+          [7, 1, 9, 2],
+          [3, 4, 7, 6]]
+
+MAT2_1 =  [[20.3, 1.6],
+           [18.6, 1.3],
+           [12.1, 1],
+           [23, 1.8]]
+
+random.seed(2024)
 
 def random_mat(n, m):
     mat = []
@@ -23,17 +35,15 @@ def identity_mat(n):
         one_index += 1
     return mat
 
-def product_of_matrices(mat1, mat2):
-    index_c = 0
-    index_r = 0
+def product_of_mat(mat1, mat2):
     res_mat = []
-    for i in range(len(mat1[0])):
-        res_mat.append([0 for j in range(mat2)])
-    for i in range(len(mat1[0])):
-        for j in range(len(mat2)):
-            
-            index_c += 1
-        index_r += 1
+    for i in range(len(mat1)):
+        res_mat.append([0 for j in range(len(mat2[0]))])
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            for k in range(len(mat2)):
+                res_mat[i][j] += mat1[i][k] * mat2[k][j]
+    return res_mat
 
 
 def show_matrix(mat):
@@ -44,3 +54,9 @@ if __name__ == "__main__":
     show_matrix(random_mat(3,4))
     print("")
     show_matrix(identity_mat(5))
+    print("")
+    show_matrix(product_of_mat(MAT1_1, MAT2_1))
+    print("")
+    show_matrix((product_of_mat(MAT1, MAT2)))
+    print("")
+    show_matrix((product_of_mat(random_mat(3,4), random_mat(4,5))))
