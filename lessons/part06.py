067bd69b93ddb3cbe6023dff7fcf35410d2cd574
may-qwer <may0220@yandex.ru>
2024-09-13 18:32:05 +0300

    page 297

--- lessons/part06.py
+++ lessons/part06.py
@@ -1,17 +1,29 @@
-class Temp:
-    name = "Temp"
+# class Temp:
+#     name = "Temp"
+#
+#     def __init__(self, n):
+#         self.nickname = n
+#
+#     def make_name(self):
+#         Temp.name = Temp.name + self.nickname
+#
+#
+# if __name__ == "__main__":
+#     green = Temp("green")
+#     print(green.nickname)
+#     print(green.name)
+#     green.make_name()
+#     print(green.name)
+#     print(Temp.name)
 
-    def __init__(self, n):
-        self.nickname = n
+class Myclass:
+    pass
 
-    def make_name(self):
-        Temp.name = Temp.name + self.nickname
+def hello():
+    print("hello word!")
 
 
 if __name__ == "__main__":
-    green = Temp("green")
-    print(green.nickname)
-    print(green.name)
-    green.make_name()
-    print(green.name)
-    print(Temp.name)
+    A = Myclass()
+    A.say = hello
+    A.say()
